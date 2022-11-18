657aed0c74f06b33c0610837237323e0b531519c
Léo Dubosclard <leo.dubosclard@epitech.eu>
2022-11-18 23:16:17 +0100

    feat: side scene + globalConfig

--- models/config.ts
+++ models/config.ts
@@ -2,5 +2,6 @@ export interface globalConfig {
     sprite_size: number; // Sprite size
     platformer: boolean; // Is plateform scene or upper scene
     gravity: number;
-    movement_speed: number;
+    player_speed: number;
+    current_layer: number;
 }
