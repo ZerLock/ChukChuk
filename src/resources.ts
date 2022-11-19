a3b5ad53afe8d4f535b3a0601da6473d67ecaa08
Joshua Brionne <joshua.brionne@iexec.ec>
2022-11-19 13:28:05 +0100

    [ADD] ressource optimize + sprites rotat

--- src/resources.ts
+++ src/resources.ts
@@ -1,24 +1,51 @@
-import * as ex from 'excalibur';
-import { Global } from './class/global';
+import * as ex from "excalibur";
+import { Global } from "./class/global";
 
 const Images: { [key: string]: ex.ImageSource } = {
-    player: new ex.ImageSource('./chuck.png'),
+  player: new ex.ImageSource("./chuck.png"),
+  playerUpper: new ex.ImageSource("./upper.png"),
 };
 
-const playerSpriteSheet = ex.SpriteSheet.fromImageSource({
-    image: Images.player,
-    grid: {
-        rows: 1,
-        columns: 10,
-        spriteHeight: Global.globalConfig.sprite_size,
-        spriteWidth: Global.globalConfig.sprite_size,
-    },
+const SidePlayerSpriteSheet = ex.SpriteSheet.fromImageSource({
+  image: Images.player,
+  grid: {
+    rows: 1,
+    columns: 10,
+    spriteHeight: Global.globalConfig.sprite_size,
+    spriteWidth: Global.globalConfig.sprite_size,
+  },
+});
+
+const UpperPlayerSpriteSheet = ex.SpriteSheet.fromImageSource({
+  image: Images.playerUpper,
+  grid: {
+    rows: 4,
+    columns: 4,
+    spriteHeight: Global.globalConfig.sprite_size,
+    spriteWidth: Global.globalConfig.sprite_size,
+  },
+});
+
+const UpperPlayerSpriteSheetStopped = ex.SpriteSheet.fromImageSource({
+  image: Images.playerUpper,
+  grid: {
+    rows: 4,
+    columns: 1,
+    spriteHeight: Global.globalConfig.sprite_size,
+    spriteWidth: Global.globalConfig.sprite_size,
+  },
 });
 
 const loader = new ex.Loader();
-const allResources = {...Images};
+const allResources = { ...Images };
 for (const res in allResources) {
-    loader.addResource(allResources[res]);
+  loader.addResource(allResources[res]);
 }
 
-export { Images, loader, playerSpriteSheet };
+export {
+  Images,
+  loader,
+  SidePlayerSpriteSheet,
+  UpperPlayerSpriteSheet,
+  UpperPlayerSpriteSheetStopped,
+};
