4c6a379b68602cc75a86a9e4dc9d6ddc86596b77
MathiDEV <mathias74.andre@gmail.com>
2022-11-19 12:06:40 +0100

    :sparkles: Fixed index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import * as ex from 'excalibur';
 import { MainGame } from "./class/game";
-import { UpperScene } from "./scene/UpperScene";
-import map from "./maps/map1.json";
-import { getSpritesToDisplay, Views } from "./utils/map";
-
-const JoshuLayers = getSpritesToDisplay(map, Views.Upper);
-
-console.log(JoshuLayers);
-
 import { loader } from './resources';
+
 
 loader.playButtonText = 'Start ChukChuk';
 
