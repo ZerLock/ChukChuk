3787adc37bf23fccef0e53944e7143be71bd9a86
MathiDEV <mathias74.andre@gmail.com>
2022-11-19 11:28:51 +0100

    Merged tes os :sparkles:

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import * as ex from 'excalibur';
 import { MainGame } from "./class/game";
+import { UpperScene } from "./scene/UpperScene";
+import map from "./maps/map1.json";
+import { getSpritesToDisplay, Views } from "./utils/map";
+
+const JoshuLayers = getSpritesToDisplay(map, Views.Upper);
+
+console.log(JoshuLayers);
 
 import { loader } from './resources';
 
